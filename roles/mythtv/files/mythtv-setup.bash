9d8555f78bf85b574e9856dac5459c1766b72be4
Martin Janser <martin@duss-janser.ch>
2015-02-06 18:53:58 +0100

    Mount cache/tmp directory of mythtv as tmpfs

--- roles/mythtv/files/mythtv-setup.bash
+++ roles/mythtv/files/mythtv-setup.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo -u mythtv sh -c "xauth add $(xauth list $DISPLAY) && mythtv-setup"
+sudo -u mythtv sh -c "xauth add $(xauth list $DISPLAY) && mythtv-setup; rm -rf ~/.Xauthority >/dev/null 2>&1"
